c9c6ff62d6ea4cf6e760c8f6130c76bede3a54d6
itsApurba <apurba.apurbaadhikary@gmail.com>
2024-03-09 19:08:17 +0530

    Fix: Header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,21 +2,25 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { logout } from "@/app/actions";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className='flex items-center max-w-screen-md p-8 justify-between m-auto'>
-      <div className='rounded-full bg-green-600 p-2'>
-        <h2 className='text-xl font-semibold tracking-tight transition-colors first:mt-0'>EM</h2>
+    <div className='p-4  backdrop-filter backdrop-blur-lg bg-opacity-30 border-b-2 border-gray-200 border-opacity-10'>
+      <div className='m-auto flex items-center  max-w-screen-md justify-between'>
+          <div className='rounded-full bg-green-600 p-2'>
+            <h2 className='text-xl font-semibold tracking-tight transition-colors first:mt-0'>EM</h2>
+          </div>
+        <Button
+          onClick={async () => {
+            console.log("logging out");
+            logout();
+          }}
+          variant={"destructive"}
+        >
+          Logout
+        </Button>
       </div>
-      <Button
-        onClick={async () => {
-          console.log("logging out");
-          logout();
-        }}
-      >
-        Logout
-      </Button>
     </div>
   );
 };
